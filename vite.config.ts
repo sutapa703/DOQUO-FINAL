27204e9b04c667a078b076e453a730087611ff6d
Sutapa Maity <sutapam510@gmail.com>
2025-06-26 21:01:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  server: {
-    port: 3000,
-    open: true
-  }
 })
